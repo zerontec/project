0c6bd2f07838f01461d12cd7a1ecfc864ccf3ed8
zerontec <zerontecnologia1@gmail.com>
2018-02-07 17:02:38 -0400

    Iniciando App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule } from 'angularfire2/database';
+import {environment} from '../environments/environment';
 
 
 import { AppComponent } from './app.component';
 
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeesComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBa-8_J1-pGifHcywiaGaMUpx2j38i-His",
+    authDomain: "angular5f-8af0c.firebaseapp.com",
+    databaseURL: "https://angular5f-8af0c.firebaseio.com",
+    projectId: "angular5f-8af0c",
+    storageBucket: "angular5f-8af0c.appspot.com",
+    messagingSenderId: "200598337474"
+  };
 };
